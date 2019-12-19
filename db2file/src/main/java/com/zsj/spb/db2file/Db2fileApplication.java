b06f8c5502d38177df0dbde6080c65346e331a57
cxyzsj <945892500@qq.com>
2019-12-19 15:43:25 +0800

    db2file 项目搭建

---
+++ db2file/src/main/java/com/zsj/spb/db2file/Db2fileApplication.java
@@ -0,0 +1,13 @@
+package com.zsj.spb.db2file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2fileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Db2fileApplication.class, args);
+    }
+
+}
---
+++ db2file/src/test/java/com/zsj/spb/db2file/Db2fileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsj.spb.db2file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db2fileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
